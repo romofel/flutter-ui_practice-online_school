042c46dc0221034fe112f696e896baeade28e07a
Felipe Romo <felipe@romofel.dev>
2021-06-22 20:35:50 -0500

    Add teacher data class

---
+++ lib/models/teacher.dart
@@ -0,0 +1,11 @@
+class Teacher {
+  final String name;
+  final String title;
+  final int badgeNumber;
+
+  const Teacher({
+    required this.name,
+    required this.title,
+    required this.badgeNumber,
+  });
+}
