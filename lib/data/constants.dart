529a328636dd31be810ce7f91e1017b69693dacd
Felipe Romo <felipe@romofel.dev>
2021-06-22 23:06:38 -0500

    Create a file with consts
    Add the linear gradients that we'll be using in the tutor list

---
+++ lib/data/constants.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/painting.dart';
+
+final gradients = <LinearGradient>[
+  const LinearGradient(
+    colors: [
+      Color(0xff8E68FE),
+      Color(0xff9F7EFF),
+      Color(0xffA181FF),
+    ],
+  ),
+  LinearGradient(
+    colors: [
+      const Color(0xffFFA632),
+      const Color(0xffFDA635),
+      const Color(0xffFFA939).withOpacity(.74),
+    ],
+  ),
+  const LinearGradient(
+    colors: [
+      Color(0xff6E8CEA),
+      Color(0xff7D99F0),
+      Color(0xff6E8CEA),
+    ],
+  ),
+];
