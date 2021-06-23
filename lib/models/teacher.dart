90a09245122a2a0c87837b50bdbb03778651e405
Felipe Romo <felipe@romofel.dev>
2021-06-22 20:36:28 -0500

    Update class to accept an image url string

--- lib/models/teacher.dart
+++ lib/models/teacher.dart
@@ -2,10 +2,12 @@ class Teacher {
   final String name;
   final String title;
   final int badgeNumber;
+  final String image;
 
   const Teacher({
     required this.name,
     required this.title,
     required this.badgeNumber,
+    required this.image,
   });
 }
