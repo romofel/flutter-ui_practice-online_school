d27f56f906e6e68ad9d754f5e8a907dc87dc68b4
Felipe Romo <felipe@romofel.dev>
2021-06-22 20:41:46 -0500

    Add list of teachers for data

---
+++ lib/data/teachers.dart
@@ -0,0 +1,58 @@
+import 'package:online_school/models/teacher.dart';
+
+const teachers = <Teacher>[
+  Teacher(
+    name: 'Easin Arafat',
+    title: 'UI/UX',
+    badge: '',
+    image: 'assets/images/teacher1.png',
+  ),
+  Teacher(
+    name: 'Miss Cathe',
+    title: 'Marketing',
+    badge: '',
+    image: 'assets/images/teacher2.png',
+  ),
+  Teacher(
+    name: 'Miss Urma',
+    title: '3D Illustrations',
+    badge: '',
+    image: 'assets/images/teacher3.png',
+  ),
+  Teacher(
+    name: 'Miss Emilia',
+    title: 'UI/UX',
+    badge: '1st',
+    image: 'assets/images/teacher4.png',
+  ),
+  Teacher(
+    name: 'Mehade Hasan',
+    title: 'UI/UX',
+    badge: '2nd',
+    image: 'assets/images/teacher5.png',
+  ),
+  Teacher(
+    name: 'Miss Fiona',
+    title: 'UI/UX',
+    badge: '3rd',
+    image: 'assets/images/teacher6.png',
+  ),
+  Teacher(
+    name: 'Miss Jolie',
+    title: 'UI/UX',
+    badge: '4th',
+    image: 'assets/images/teacher7.png',
+  ),
+  Teacher(
+    name: 'Miss Lillian',
+    title: 'UI/UX',
+    badge: '5th',
+    image: 'assets/images/teacher8.png',
+  ),
+  Teacher(
+    name: 'Miss Natalia',
+    title: 'UI/UX',
+    badge: '6th',
+    image: 'assets/images/teacher9.png',
+  ),
+];
