a8a6b47aba9df65d2bc256209f58c414d4304ce1
Felipe Romo <felipe@romofel.dev>
2021-06-22 20:51:04 -0500

    Refactor sections to their own build methods

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -11,115 +11,10 @@ class HomeScreen extends StatelessWidget {
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            Padding(
-              padding:
-                  const EdgeInsets.symmetric(horizontal: 24.0, vertical: 16.0),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                crossAxisAlignment: CrossAxisAlignment.end,
-                children: [
-                  const Icon(Icons.menu),
-                  Container(
-                    width: 40,
-                    height: 40,
-                    decoration: BoxDecoration(
-                      color: const Color(0xffEFEFEF),
-                      borderRadius: BorderRadius.circular(10),
-                    ),
-                    child: const Image(
-                      image: AssetImage('assets/images/avatar.png'),
-                      fit: BoxFit.cover,
-                    ),
-                  ),
-                ],
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 24.0),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(
-                    'Hello Masum',
-                    style: GoogleFonts.lexendExa(
-                      color: Colors.black,
-                      fontSize: 20,
-                      fontWeight: FontWeight.w600,
-                    ),
-                  ),
-                  Text('Welcome to online school',
-                      style: GoogleFonts.lexendExa(
-                        color: const Color(0xff260404).withOpacity(.70),
-                        fontSize: 12,
-                        fontWeight: FontWeight.w300,
-                      )),
-                ],
-              ),
-            ),
+            _buildAppBar(),
+            _buildHeader(),
             const SizedBox(height: 24),
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 24),
-              child: Stack(
-                alignment: AlignmentDirectional.bottomStart,
-                children: [
-                  Container(
-                    height: 168,
-                    padding: const EdgeInsets.symmetric(horizontal: 24),
-                    decoration: BoxDecoration(
-                      color: const Color(0xff00BBC7),
-                      borderRadius: BorderRadius.circular(15),
-                    ),
-                    child: Row(
-                      children: [
-                        const Spacer(),
-                        Expanded(
-                          child: Column(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            crossAxisAlignment: CrossAxisAlignment.end,
-                            children: [
-                              Text(
-                                'What do you want to learn today?',
-                                textAlign: TextAlign.right,
-                                style: GoogleFonts.lexendExa(
-                                  color: Colors.white,
-                                  fontSize: 14,
-                                  fontWeight: FontWeight.w500,
-                                ),
-                              ),
-                              const SizedBox(height: 24),
-                              Container(
-                                padding: const EdgeInsets.symmetric(
-                                    horizontal: 24, vertical: 16),
-                                decoration: BoxDecoration(
-                                  color: Colors.white,
-                                  borderRadius: BorderRadius.circular(43),
-                                ),
-                                child: Text(
-                                  'Get Started',
-                                  style: GoogleFonts.lexendExa(
-                                    color: Colors.black,
-                                    fontSize: 11,
-                                    fontWeight: FontWeight.w500,
-                                  ),
-                                ),
-                              ),
-                            ],
-                          ),
-                        ),
-                      ],
-                    ),
-                  ),
-                  const Padding(
-                    padding: EdgeInsets.only(left: 24),
-                    child: Image(
-                      image: AssetImage('assets/images/hero_girl.png'),
-                      fit: BoxFit.cover,
-                      height: 200,
-                    ),
-                  ),
-                ],
-              ),
-            ),
+            _buildHeroSection(),
             const SizedBox(height: 24),
             Padding(
               padding: const EdgeInsets.symmetric(horizontal: 24),
@@ -134,6 +29,122 @@ class HomeScreen extends StatelessWidget {
             ),
           ],
         ),
+      ),
+    );
+  }
+
+  Padding _buildHeroSection() {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 24),
+      child: Stack(
+        alignment: AlignmentDirectional.bottomStart,
+        children: [
+          Container(
+            height: 168,
+            padding: const EdgeInsets.symmetric(horizontal: 24),
+            decoration: BoxDecoration(
+              color: const Color(0xff00BBC7),
+              borderRadius: BorderRadius.circular(15),
+            ),
+            child: Row(
+              children: [
+                const Spacer(),
+                Expanded(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.end,
+                    children: [
+                      Text(
+                        'What do you want to learn today?',
+                        textAlign: TextAlign.right,
+                        style: GoogleFonts.lexendExa(
+                          color: Colors.white,
+                          fontSize: 14,
+                          fontWeight: FontWeight.w500,
+                        ),
+                      ),
+                      const SizedBox(height: 24),
+                      Container(
+                        padding: const EdgeInsets.symmetric(
+                            horizontal: 24, vertical: 16),
+                        decoration: BoxDecoration(
+                          color: Colors.white,
+                          borderRadius: BorderRadius.circular(43),
+                        ),
+                        child: Text(
+                          'Get Started',
+                          style: GoogleFonts.lexendExa(
+                            color: Colors.black,
+                            fontSize: 11,
+                            fontWeight: FontWeight.w500,
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+          const Padding(
+            padding: EdgeInsets.only(left: 24),
+            child: Image(
+              image: AssetImage('assets/images/hero_girl.png'),
+              fit: BoxFit.cover,
+              height: 200,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Padding _buildHeader() {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 24.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            'Hello Masum',
+            style: GoogleFonts.lexendExa(
+              color: Colors.black,
+              fontSize: 20,
+              fontWeight: FontWeight.w600,
+            ),
+          ),
+          Text('Welcome to online school',
+              style: GoogleFonts.lexendExa(
+                color: const Color(0xff260404).withOpacity(.70),
+                fontSize: 12,
+                fontWeight: FontWeight.w300,
+              )),
+        ],
+      ),
+    );
+  }
+
+  Padding _buildAppBar() {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 24.0, vertical: 16.0),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        crossAxisAlignment: CrossAxisAlignment.end,
+        children: [
+          const Icon(Icons.menu),
+          Container(
+            width: 40,
+            height: 40,
+            decoration: BoxDecoration(
+              color: const Color(0xffEFEFEF),
+              borderRadius: BorderRadius.circular(10),
+            ),
+            child: const Image(
+              image: AssetImage('assets/images/avatar.png'),
+              fit: BoxFit.cover,
+            ),
+          ),
+        ],
       ),
     );
   }
