e2713878630ec3b05b8cac2e41002341d54b6c18
Felipe Romo <felipe@romofel.dev>
2021-06-24 01:21:32 -0500

    Add padding to the row

--- lib/screens/class_screen.dart
+++ lib/screens/class_screen.dart
@@ -30,70 +30,72 @@ class ClassScreen extends StatelessWidget {
           ),
           Positioned(
             top: 56,
-            left: 24,
-            child: Row(
-              children: [
-                Container(
-                  decoration: BoxDecoration(
-                    color: const Color(0xffC4C4C4),
-                    borderRadius: BorderRadius.circular(10),
-                  ),
-                  child: const Image(
-                    image: AssetImage('assets/images/avatar.png'),
-                    fit: BoxFit.cover,
-                  ),
-                ),
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(
-                      'Miss Seraphina',
-                      style: GoogleFonts.lexendExa(
-                        color: Colors.black,
-                        fontSize: 14,
-                        fontWeight: FontWeight.w500,
-                      ),
+            child: Padding(
+              padding: const EdgeInsets.all(24),
+              child: Row(
+                children: [
+                  Container(
+                    decoration: BoxDecoration(
+                      color: const Color(0xffC4C4C4),
+                      borderRadius: BorderRadius.circular(10),
                     ),
-                    Text(
-                      'UI/UX Instructor',
-                      style: GoogleFonts.lexendExa(
-                        color: Colors.black.withOpacity(.6),
-                        fontSize: 11,
-                        fontWeight: FontWeight.w400,
-                      ),
+                    child: const Image(
+                      image: AssetImage('assets/images/avatar.png'),
+                      fit: BoxFit.cover,
                     ),
-                  ],
-                ),
-                Container(
-                  padding:
-                      const EdgeInsets.symmetric(horizontal: 8, vertical: 4),
-                  decoration: BoxDecoration(
-                    color: Colors.white,
-                    borderRadius: BorderRadius.circular(25),
                   ),
-                  child: Row(
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
                     children: [
-                      Container(
-                        width: 8,
-                        height: 8,
-                        decoration: const BoxDecoration(
-                          color: Color(0xffFF3E3E),
-                          shape: BoxShape.circle,
+                      Text(
+                        'Miss Seraphina',
+                        style: GoogleFonts.lexendExa(
+                          color: Colors.black,
+                          fontSize: 14,
+                          fontWeight: FontWeight.w500,
                         ),
                       ),
-                      const SizedBox(width: 4),
                       Text(
-                        'Live',
+                        'UI/UX Instructor',
                         style: GoogleFonts.lexendExa(
-                          color: const Color(0xffFF2222),
+                          color: Colors.black.withOpacity(.6),
                           fontSize: 11,
                           fontWeight: FontWeight.w400,
                         ),
                       ),
                     ],
                   ),
-                ),
-              ],
+                  Container(
+                    padding:
+                        const EdgeInsets.symmetric(horizontal: 8, vertical: 4),
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(25),
+                    ),
+                    child: Row(
+                      children: [
+                        Container(
+                          width: 8,
+                          height: 8,
+                          decoration: const BoxDecoration(
+                            color: Color(0xffFF3E3E),
+                            shape: BoxShape.circle,
+                          ),
+                        ),
+                        const SizedBox(width: 4),
+                        Text(
+                          'Live',
+                          style: GoogleFonts.lexendExa(
+                            color: const Color(0xffFF2222),
+                            fontSize: 11,
+                            fontWeight: FontWeight.w400,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
             ),
           ),
         ],
